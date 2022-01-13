df3dd3970afebcb0a403d60fcb89751f4779f3fc
Blessed Sibanda <blessedsibanda.me@gmail.com>
2022-01-13 07:32:54 +0200

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create! name: "Blessed Sibanda", email: "blessed@example.com", password: "secret1"
+User.create! name: "Pauline", email: "pau@example.com", password: "secret2"
+User.create! name: "Rose", email: "rose@example.com", password: "secret3"
